ae306bf6b4b6e4a2bd433345a3a65a24fbd51c35
William Pepple <williampepple1@gmail.com>
2024-03-04 13:52:14 +0100

    Update routes.go
    Include downlaodExpensesAsCSV route

--- routes/routes.go
+++ routes/routes.go
@@ -9,6 +9,7 @@ import (
 
 func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 
+	r.GET("/download", handlers.DownloadExpensesAsCSV(db))
 	r.GET("/expenses", handlers.ListExpenses(db))
 	r.POST("/expenses", handlers.CreateExpense(db))
 	r.GET("/expenses/:id", handlers.GetExpense(db))
