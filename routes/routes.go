271286868b5f71af19bed1f1a97e817b84e3fb8f
William Pepple <williampepple1@gmail.com>
2024-03-04 13:24:13 +0100

    Create routes.go
    Create the route for expenses

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"golang-sheets/handlers"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupRoutes(r *gin.Engine, db *gorm.DB) {
+
+	r.GET("/expenses", handlers.ListExpenses(db))
+	r.POST("/expenses", handlers.CreateExpense(db))
+	r.GET("/expenses/:id", handlers.GetExpense(db))
+	r.PUT("/expenses/:id", handlers.UpdateExpense(db))
+	r.DELETE("/expenses/:id", handlers.DeleteExpense(db))
+
+}
